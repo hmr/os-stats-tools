b9a62424eada611a251ffa0ef06a528a25ed2aab
ubuntu@mh-rp4-1 <ubuntu@mh-rp4-1>
2020-12-25 09:59:17 +0900

    [print_mpressure] apply for linux

--- src/print_mpressure/print_mpressure.bash
+++ src/print_mpressure/print_mpressure.bash
@@ -1,19 +1,34 @@
 #!/bin/bash
 
-WIRED=5     # Pages wired down
-CMPRSSOR=15 # Pages occupied by compressor
-#vm_stat -c 1 1 | pbcopy;
-#pbpaste;
-#VMSTATS=($(pbpaste | tail -n 1))
-VMSTATS=($(vm_stat -c 1 1 | tail -n 1))
 
-#echo wired: ${VMSTATS[$WIRED]}
-#echo cmprssor: ${VMSTATS[$CMPRSSOR]}
-#echo -n "wired+cmprssor(KiB): "
-#echo "( ${VMSTATS[$WIRED]} + ${VMSTATS[$CMPRSSOR]} ) * 4" | bc
-#echo "scale=4; ( ${VMSTATS[$WIRED]} + ${VMSTATS[$CMPRSSOR]} ) * 4 / ($(sysctl hw.memsize | cut -d' ' -f2) / 1024) * 100" | bc
-#MEM_PRESS=$(echo "scale=4; (${VMSTATS[$WIRED]} + ${VMSTATS[$CMPRSSOR]}) / ($(sysctl hw.memsize | cut -d' ' -f2) / 4096) * 100" | bc)
-MEM_PRESS=$(echo "scale=7; (${VMSTATS[$WIRED]} + ${VMSTATS[$CMPRSSOR]}) / ($(sysctl hw.memsize | cut -d' ' -f2) ) * 409600" | bc)
-printf '%.1f\n' "$MEM_PRESS"
+_SYSTEM=$(uname -s | tr "[A-Z]" "[a-z]")
+
+case "${_SYSTEM}" in
+
+  "darwin" )
+    WIRED=5     # Pages wired down
+    CMPRSSOR=15 # Pages occupied by compressor
+    #vm_stat -c 1 1 | pbcopy;
+    #pbpaste;
+    #VMSTATS=($(pbpaste | tail -n 1))
+    VMSTATS=($(vm_stat -c 1 1 | tail -n 1))
+
+    #echo wired: ${VMSTATS[$WIRED]}
+    #echo cmprssor: ${VMSTATS[$CMPRSSOR]}
+    #echo -n "wired+cmprssor(KiB): "
+    #echo "( ${VMSTATS[$WIRED]} + ${VMSTATS[$CMPRSSOR]} ) * 4" | bc
+    #echo "scale=4; ( ${VMSTATS[$WIRED]} + ${VMSTATS[$CMPRSSOR]} ) * 4 / ($(sysctl hw.memsize | cut -d' ' -f2) / 1024) * 100" | bc
+    #MEM_PRESS=$(echo "scale=4; (${VMSTATS[$WIRED]} + ${VMSTATS[$CMPRSSOR]}) / ($(sysctl hw.memsize | cut -d' ' -f2) / 4096) * 100" | bc)
+    MEM_PRESS=$(echo "scale=7; (${VMSTATS[$WIRED]} + ${VMSTATS[$CMPRSSOR]}) / ($(sysctl hw.memsize | cut -d' ' -f2) ) * 409600" | bc)
+    printf '%.1f\n' "$MEM_PRESS"
+    ;;
+
+  "linux" )
+
+    read ALLMEM AVAMEM  <<< $(cat /proc/meminfo | grep -P "MemTotal|MemAvailable" | awk '{printf("%s ", $2);}')
+    printf '%.1f\n' "$(echo "scale=7;100-($AVAMEM/$ALLMEM)*100" | bc)"
+    ;;
+
+  esac
 
 # vim: ft=bash ts=2 sw=2 et fenc=utf-8
