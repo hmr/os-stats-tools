32b168c3e75e8ddd5e56193b215b4b2220ff1109
hmr@entropy4 <hmr@entropy4.local>
2023-09-28 22:31:13 +0900

    [uptime] Support universal binary

--- src/print_uptime/makebin.sh
+++ src/print_uptime/makebin.sh
@@ -10,13 +10,17 @@ fi
 case "$(uname -s)" in
     "Linux" )
         echo "Making binary for Linux."
-         gcc -D_LINUX ${OPT_ARG} print_uptime.c -o print_uptime -lm
-         ;;
+        gcc -D_LINUX "${OPT_ARG}" print_uptime.c -o print_uptime -lm
+        ;;
 
-     "Darwin" )
+    "Darwin" )
         echo "Making binary for macOS."
-         cc  -D_MACOS ${OPT_ARG} print_uptime.c -o print_uptime -lm
-         ;;
+        # Make universal binary
+        # cc  -arch arm64 -D_MACOS "${OPT_ARG}" -o print_uptime.arm64 -lm print_uptime.c
+        # cc  -arch x86_64 -D_MACOS "${OPT_ARG}" -o print_uptime.x86_64 -lm print_uptime.c
+        # lipo -create -output print_uptime print_uptime.arm64 print_uptime.x86_64
+        cc  -arch arm64 -arch x86_64 -D_MACOS "${OPT_ARG}" -o print_uptime -lm print_uptime.c
+        ;;
 esac
 
 [ $? -eq 0 ] && ./print_uptime
